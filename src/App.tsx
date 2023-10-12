928a698fb83397e74adf2cd8ec4a53de8d2923e7
ImanolBraccciale <imanol11699@outlook.com>
2023-10-12 03:10:02 -0300

    aaaaaaaa

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Projects from "./Projects/Projects"
+import Skills from "./Skills/Skills";
+import Contact from "./Contact/Contact";
+import Home from "./Home/Home"
 
-function App() {
+export const App : React.FC<{}>=()=> {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+   <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Projects" element={<Projects />} />
+          <Route path="/Skills" element={<Skills />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
     </div>
   );
 }
 
-export default App;
---
+++ src/Projects/Projects.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import NavBar from "../components/NavBar/NavBar"
+
+const Projects: React.FC = ()=>{
+    
+    return(
+        <div>
+            <NavBar/>
+        </div>
+    )
+}
+export default Projects;
