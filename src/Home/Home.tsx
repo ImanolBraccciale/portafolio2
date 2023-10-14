214de315f73ef2d3d88323d685c96effd1862d78
ImanolBraccciale <imanol11699@outlook.com>
2023-10-14 02:01:55 -0300

    monedas listas

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,25 +1,29 @@
 import React from "react"
 import NavBar from "../components/NavBar/NavBar"
 import "../assets/Home.css"
- 
-const Home: React.FC<{}> = ()=>{
-    
-    return(
-    <div>
-        <NavBar/>
-        <div className="containerHoome">
-        <div className="containerPacman">
-            <div className="eclipse"/>
-            <div className="poligono"/>
-        </div>
-        <div className="coinContainer">
-     
-  
-            <p></p>
-            <p></p>
-        </div>
-     </div>
-     </div>)
+
+const Home: React.FC<{}> = () => {
+
+    return (
+        <div>
+            <NavBar />
+            <div className="containerHoome">
+                <div className="containerPacman">
+                    <div className="eclipse" />
+                    <div className="poligono" />
+                </div>
+                <p>
+                    Hola a todos!
+                    Soy Imanol Bracciale, desarrollador de páginas web, y estoy emocionado de compartir mi portafolio contigo hoy.
+                    Gracias por ingresar a mi portafolio para conocer mi trabajo y experiencia.
+                </p>
+                <div className="Coins" />
+
+                <p></p>
+
+            </div>
+
+        </div>)
 }
 
 export default Home
