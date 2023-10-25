48a5b918a03f6cbcec9135933edeb967948284fa
ImanolBraccciale <imanol11699@outlook.com>
2023-10-25 14:23:15 -0300

    about en proceso

--- src/About/About.tsx
+++ src/About/About.tsx
@@ -2,8 +2,11 @@ import React from "react"
 import NavBar from "../components/NavBar/NavBar"
  import "../assets/About.css"
 
+
  const  About: React.FC<{}> = () => {
-    
+    const redirigir = (url:string) => {
+        window.open(  url, "_blank", "noopener,noreferrer");
+      }
     return(
         <div className="containerAbout">
             <NavBar/>
@@ -11,6 +14,18 @@ import NavBar from "../components/NavBar/NavBar"
             <div className="cloud" style={{ marginLeft: "15vw" }}/>
             <div className="cloud" style={{ marginLeft: "75vw" }}/>
             <div className="brick"/>
+            <p className="p">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Magni enim exercitationem sit itaque, nobis eligendi qui cupiditate, culpa 
+            totam debitis ipsam dolor placeat excepturi nihil minus voluptas similique 
+            suscipit hic!</p>
+            <div className="containerButton">
+
+             <button className="button linkedin" onClick={()=>{redirigir("https://www.linkedin.com/in/imanol-bracciale-2a5625206/")}} ></button>
+             <button className="button" onClick={()=>{redirigir("https://github.com/ImanolBraccciale")}} >Github</button>
+            </div>
+        
+           
+            
         </div>
     )
     }
