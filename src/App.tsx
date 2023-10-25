d8ff8aa7fa70e93f13c12d726b90565c4a867afd
ImanolBraccciale <imanol11699@outlook.com>
2023-10-25 17:37:51 -0300

    contact cancelado, home hecho, faltan detalles

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Projects from "./Projects/Projects"
 import Skills from "./Skills/Skills";
-import Contact from "./Contact/Contact";
 import Home from "./Home/Home"
 import About from "./About/About";
 
@@ -16,7 +15,6 @@ import About from "./About/About";
           <Route path="/" element={<Home />} />
           <Route path="/Projects" element={<Projects />} />
           <Route path="/Skills" element={<Skills />} />
-          <Route path="/Contact" element={<Contact />} />
           <Route path="/About" element={<About />} />
         </Routes>
       </div>
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -6,15 +6,16 @@ const NavBar: React.FC<{}> = () => {
   return (
     <div className="container">
       <div className="navButtons">
+        <Link to="/" className="navLink collision-div" >
+          Home
+        </Link>
         <Link to="/Skills" className="navLink  collision-div"  >
           Skills
         </Link>
         <Link to="/Projects" className="navLink collision-div"  >
           Projects
         </Link>
-        <Link to="/Contact" className="navLink collision-div" >
-          Contact
-        </Link>
+ 
         <Link to="/About" className="navLink collision-div"  >
           About Me
         </Link>
