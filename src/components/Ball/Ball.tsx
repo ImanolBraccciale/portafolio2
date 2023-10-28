3e44df2f6339082b75113c5722d59abe218ed919
ImanolBraccciale <imanol11699@outlook.com>
2023-10-28 19:23:19 -0300

    asdasd

--- src/components/Ball/Ball.tsx
+++ src/components/Ball/Ball.tsx
@@ -20,10 +20,10 @@ const Ball: React.FC = () => {
 
   // Referencia al elemento <canvas>
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const collisionDivs = document.querySelectorAll(".collision-div");
+      const collisionDivs = document.querySelectorAll(".collision-div");
   // useEffect se utiliza para efectos secundarios y ciclo de vida del componente
   useEffect(() => {
-
+    // Obtiene la referencia al elemento <canvas>
     const canvas = canvasRef.current;
     if (!canvas) {
       console.error("Canvas element not found.");
