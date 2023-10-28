26d3b38d36de7706f8b1c82313c307ebb43fb1a3
ImanolBraccciale <imanol11699@outlook.com>
2023-10-28 20:51:41 -0300

    puse la referencia en el useEffect

--- src/components/Ball/Ball.tsx
+++ src/components/Ball/Ball.tsx
@@ -20,9 +20,9 @@ const Ball: React.FC = () => {
 
   // Referencia al elemento <canvas>
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
-      const collisionDivs = document.querySelectorAll(".collision-div");
   // useEffect se utiliza para efectos secundarios y ciclo de vida del componente
   useEffect(() => {
+    const collisionDivs = document.querySelectorAll(".collision-div");
     // Obtiene la referencia al elemento <canvas>
     const canvas = canvasRef.current;
     if (!canvas) {
